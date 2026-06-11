f30a442ef1ffd8e48bd9f3a1a957a21da427ec2e
Sreejith S <sreejithsureshkumar7@gmail.com>
2026-06-11 23:15:06 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,10 +5,11 @@
 // 4. Add Firebase SDK scripts in index.html if you want real database
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+   apiKey: "AIzaSyC58hyVdJsPUQZ9ZTWxfKRhaBvZ9ijPh-s",
+  authDomain: "ai-data-dashboard-12eb0.firebaseapp.com",
+  projectId: "ai-data-dashboard-12eb0",
+  storageBucket: "ai-data-dashboard-12eb0.firebasestorage.app",
+  messagingSenderId: "84116753531",
+  appId: "1:84116753531:web:24b213767b3b5664f626de",
+  measurementId: "G-TC68N228TV"
 };
